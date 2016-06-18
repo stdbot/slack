f2d20f8e6e6ebf180f2c40939bd059c2f4822542
Valérian Galliat <val@codejam.info>
2016-06-18 15:45:16 -0400

    Fix

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function Slack (config) {
   emitter.address = (user, text) => `${emitter.mention(user)}: ${text}`
 
   emitter.mentions = message =>
-    (message.raw.match(/<@[^>]+>/g) || [])
+    (message.raw.text.match(/<@[^>]+>/g) || [])
       .map(tag => tag.slice(2, -1))
       .map(id => state.users[id])
 
